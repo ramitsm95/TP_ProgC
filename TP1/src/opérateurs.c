1a400d354328632be9fb716ff28f8c2014c91e95
ramitsm95 <rami.aissal04@gmail.com>
2026-09-18 13:36:45 +0000

    test

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,30 @@
+#include <stdio.h>
 
+int main(void) {
+    int a = 16;
+    int b = 3;
+
+    // 1. Addition
+    printf("a + b = %d\n", a + b);
+
+    // 2. Soustraction
+    printf("a - b = %d\n", a - b);
+
+    // 3. Multiplication
+    printf("a * b = %d\n", a * b);
+
+    // 4. Division entière
+    printf("a / b = %d\n", a / b);
+
+    // 5. Modulo (reste de la division)
+    printf("a %% b = %d\n", a % b);
+
+    // 6. Égalité (0 = faux, 1 = vrai)
+    printf("a == b : %d\n", a == b);
+
+    // 7. Supériorité (0 = faux, 1 = vrai)
+    printf("a > b : %d\n", a > b);
+
+    return 0;
+}
 
